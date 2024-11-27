24ca5265a4d085a0a22d3479c78f1dd0758d3502
Jakub Kapłonek <kuba.kaplonek@gmail.com>
2024-11-28 00:14:09 +0100

    Rollback run script

--- backend/run-script.sh
+++ backend/run-script.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-DATABASE_PATH="/var/www/storage/database/database.sqlite"
-
-if [ ! -f "$DATABASE_PATH" ]; then
-    echo "Baza danych nie istnieje. Wykonywanie migracji..."
-    mkdir -p /var/www/storage/database
-    touch "$DATABASE_PATH"
-    php artisan migrate
-    chown -R 1000:1000 /var/www/storage
-else
-    echo "Baza danych już istnieje. Migracja nie jest potrzebna."
-fi
-
 apache2ctl -D FOREGROUND
